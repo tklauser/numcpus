28ace8bdd3738d455dcf6137966589c939a8216f
Tobias Klauser <tklauser@distanz.ch>
2021-01-27 14:54:49 +0100

    Add solaris/illumos support

--- numcpus_unsupported.go
+++ numcpus_unsupported.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !darwin,!dragonfly,!freebsd,!linux,!netbsd,!openbsd
+// +build !darwin,!dragonfly,!freebsd,!linux,!netbsd,!openbsd,!solaris
 
 package numcpus
 
